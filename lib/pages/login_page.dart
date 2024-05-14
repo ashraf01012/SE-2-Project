81d3d84abee38fdf284e74448443c9534b1eeb57
EslamMahmood <154475033+EslamMahmood@users.noreply.github.com>
2024-05-14 18:35:38 +0300

    Update login_page.dart

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,54 +1,72 @@
-import 'package:auth_module/components/button.dart';
-import 'package:auth_module/components/text_field.dart';
-import 'package:auth_module/pages/forgot_pw_page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
+import 'package:project/components/button.dart';
+import 'package:project/pages/forgot_pw_page.dart';
+import 'package:project/components/text_field.dart';
 
 class LoginPage extends StatefulWidget {
-  final Function() ? onTap;
-  const LoginPage({super.key , required this.onTap});
+  final Function()? onTap;
+  const LoginPage({super.key, required this.onTap});
 
   @override
   State<LoginPage> createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
-
-  //text editing controllers
+  // Text editing controllers
   final emailTextController = TextEditingController();
   final passwordTextController = TextEditingController();
 
-//sign in method (onAction)
-void signIn() async{
+  // Sign in method (onAction)
+  void signIn() async {
+    // Show loading circle
+    showDialog(
+      context: context,
+      builder: (context) => const Center(child: CircularProgressIndicator()),
+    );
 
-//show loading circle
-showDialog(context: context, builder:(context) =>const Center(child: CircularProgressIndicator(),));
+    try {
+      // Sign in with email and password
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: emailTextController.text,
+        password: passwordTextController.text,
+      );
 
-  try{//if any errors while sign in
-await FirebaseAuth.instance.signInWithEmailAndPassword(
-  email: emailTextController.text, password: passwordTextController.text);
+      // Pop loading circle
+      if (context.mounted) Navigator.pop(context);
 
-//pop loading circle
-if(context.mounted) Navigator.pop(context);
-
+      // Check if the user is an admin
+      if (emailTextController.text == 'admin1@gmail.com' &&
+          passwordTextController.text == 'admin11111') {
+        // Navigate to admin page
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => const AdminPage()),
+        );
+      } else {
+        // Navigate to home page
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => const HomePage()),
+        );
+      }
+    } on FirebaseAuthException catch (e) {
+      // Pop loading circle
+      Navigator.pop(context);
+      // Display error message
+      displayMessage(e.code);
+    }
   }
-  on FirebaseAuthException catch(e){
-//pop loading circle
-Navigator.pop(context);
-//display error message
-displayMessage(e.code);
 
+  // Display a dialog message for errors
+  void displayMessage(String message) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text(message),
+      ),
+    );
   }
-}
-
-//display a dialog message while errors
-void displayMessage(String message){
-showDialog(context: context, builder:((context) => AlertDialog(
-title: Text(message),
-)),
-);
-}
 
   @override
   Widget build(BuildContext context) {
@@ -61,58 +79,83 @@ title: Text(message),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                //logo
+                // Logo
                 const Icon(
                   Icons.person,
                   size: 150,
                 ),
 
-//welcome back message
+                // Welcome back message
                 const SizedBox(height: 10),
-                const Text("Welcome back, you've been missed!",style: TextStyle(color: Colors.grey,fontWeight: FontWeight.bold),),
+                const Text(
+                  "Welcome back, you've been missed!",
+                  style: TextStyle(
+                      color: Colors.grey, fontWeight: FontWeight.bold),
+                ),
 
                 const SizedBox(
                   height: 25,
                 ),
-//email textfield
-MyTextField(
-  controller: emailTextController, hintText: 'Email', obscureText: false,),
-  //password textfield
-const SizedBox(height: 25,),
+                // Email textfield
                 MyTextField(
-  controller: passwordTextController, hintText: 'Password', obscureText: true,),
+                  controller: emailTextController,
+                  hintText: 'Email',
+                  obscureText: false,
+                ),
+                // Password textfield
+                const SizedBox(height: 25),
+                MyTextField(
+                  controller: passwordTextController,
+                  hintText: 'Password',
+                  obscureText: true,
+                ),
 
-//forgot password field
-const SizedBox(height: 10,),
-Row(
-  mainAxisAlignment: MainAxisAlignment.end,
-  children: [ GestureDetector(//to be tabble
-onTap: (){
-Navigator.push(context, MaterialPageRoute(builder: (context){
-  return const ForgotPasswordPage();
-}),);
-
-},
-    child: const Text('Forgot Password?',style: TextStyle(fontWeight: FontWeight.bold,color:Colors.blue),),
-            ),]),
-
-//sign in button
-const SizedBox(height: 25,),
-MyButton(onTap: signIn, text: 'Sign In'),
-const SizedBox(height: 20,),
-//go to register page
-Row(
-mainAxisAlignment: MainAxisAlignment.center,
-  children: [
-
-Text("Not a member?",style: TextStyle(color: Colors.grey[700],),),
-const SizedBox(width: 4,),
-GestureDetector(
-onTap: widget.onTap,
-child:
-const Text("Register now",style: TextStyle(fontWeight: FontWeight.bold,color:Colors.blue),),
-),],
-),
+                // Forgot password field
+                const SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    GestureDetector(
+                      // To be tappable
+                      onTap: () {
+                        Navigator.push(context,
+                            MaterialPageRoute(builder: (context) {
+                          return const ForgotPasswordPage();
+                        }));
+                      },
+                      child: const Text(
+                        'Forgot Password?',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, color: Colors.blue),
+                      ),
+                    ),
+                  ],
+                ),
+                // Sign in button
+                const SizedBox(height: 25),
+                MyButton(onTap: signIn, text: 'Sign In'),
+                const SizedBox(height: 20),
+                // Go to register page
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Not a member?",
+                      style: TextStyle(
+                        color: Colors.grey[700],
+                      ),
+                    ),
+                    const SizedBox(width: 4),
+                    GestureDetector(
+                      onTap: widget.onTap,
+                      child: const Text(
+                        "Register now",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, color: Colors.blue),
+                      ),
+                    ),
+                  ],
+                ),
               ],
             ),
           ),
